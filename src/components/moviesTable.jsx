2acd22b8934fa57ae15f7f07bad131c12d0b0df4
sakibcy <sakibal623@gmail.com>
2022-04-04 23:33:27 +0600

    - Sorting, Extracting Tables

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,33 +1,29 @@
 import Like from "./common/like";
 import React, {Component} from "react";
+import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 
 class MoviesTable extends Component {
-  raiseSort = path => {
-    const sortColumn = {...this.props.sortColumn};
-    if(sortColumn.path === path){
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
-    }
-    else {
-      sortColumn.order = 'asc';
-      sortColumn.path = path;
-    }
-    this.props.onSort(sortColumn);
-  }
+  columns = [
+    {path: 'title', label: 'Title'},
+    {path: 'genre.name', label: 'Genre'},
+    {path: 'numberInStock', label: 'Stock'},
+    {path: 'dailyRentalRate', label: 'Rate'},
+    {key: 'like'},
+    {key: 'delete'},
+  ];
    
   render() { 
-    const {movies, onLike, onDelete}= this.props;
+    const {movies, onLike, onDelete, onSort, sortColumn}= this.props;
 
-    return ( <table className="table">
-    <thead>
-      <tr>
-        <th onClick={() => this.raiseSort('title')}>Title</th>
-        <th onClick={() => this.raiseSort('genre.name')}>Genre</th>
-        <th onClick={() => this.raiseSort('numberInStock')}>Stock</th>
-        <th onClick={() => this.raiseSort('dailyRentalRate')}>Rate</th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
+    return ( 
+    <table className="table">
+    <TableHeader 
+    columns={this.columns}
+    onSort={onSort}
+    sortColumn={sortColumn}
+    />
+    <TableBody data={movies} columns={this.columns} />
     <tbody>
       {movies.map((movie) => (
         <tr key={movie._id}>
